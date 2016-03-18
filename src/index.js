6489402ee19c499320c72b1b3df773d2e36f2da1
Leonid Kukuyev <leonidkukuyev@gmail.com>
2016-03-18 14:06:06 -0400

    First arg is directory name to make. Make directory with mkdirp

--- src/index.js
+++ src/index.js
@@ -4,16 +4,36 @@ install();
 
 import path from 'path';
 import { ncp } from 'ncp';
+import mkdirp from 'mkdirp';
+
+var folderName = process.argv[2];
+if(!folderName){
+    folderName = 'new-es6-repo';
+}
 
 var source = path.normalize(path.join(__dirname, '..', 'repo'));
 var destination = process.cwd();
 
-console.log(`\nCopying es6-babel boilerplate directory from: \n${source}\n to: \n${destination}\n ...`);
+console.log(`folderName: ${folderName}`);
+console.log(`source: ${source}`);
+console.log(`destination: ${destination}`);
+console.log(`Creating new folder: ${folderName}`);
 
-ncp(source, destination, function (err) {
- if (err) {
-   return console.error(err);
- }
- console.log('\nCopying complete');
- console.log('run $ npm install && npm run dev\n');
+try{
+mkdirp(folderName, function(err) {
+	if (err) {
+		throw err;
+	}
+
+    console.log(`Copying es6-babel boilerplate directory from: \n${source}\n to: \n${destination}\n ...`);
+    ncp(source, destination, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('\nCopying complete');
+        console.log('run $ npm install && npm run dev\n');
+    });
 });
+} catch(e) {
+    console.log(e);
+}
