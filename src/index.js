4654b859a200ccdd1cb45f293371d4f273a497c7
Leonid Kukuyev <leonidkukuyev@gmail.com>
2016-03-18 13:44:37 -0400

    Change console.logs

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import { ncp } from 'ncp';
 var source = path.normalize(path.join(__dirname, '..', 'repo'));
 var destination = process.cwd();
 
-console.log(source);
-console.log(destination);
+console.log(`\nCopying es6-babel boilerplate directory from: \n${source}\n to: \n${destination}\n ...`);
+
 ncp(source, destination, function (err) {
  if (err) {
    return console.error(err);
  }
- console.log('done!');
+ console.log('\nCopying complete');
+ console.log('run $ npm install && npm run dev\n');
 });
